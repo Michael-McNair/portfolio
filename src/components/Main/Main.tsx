09b3d0abfb057a14b3ac019628c1df3e9b86b8fc
Michael McNair <michaeltsuyoshi@gmail.com>
2022-11-29 13:43:22 +0900

    add simple sections

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,21 +4,21 @@ import './scss/Main.css';
 export default function Main() {
   return (
     <main>
-      <main id="about-me">
+      <section id="about-me">
         <h1>About Me</h1>
-      </main>
-      <main id="skills">
+      </section>
+      <section id="skills">
         <h1>Skills</h1>
-      </main>
-      <main id="projects">
+      </section>
+      <section id="projects">
         <h1>Projects</h1>
-      </main>
-      <main id="experience">
+      </section>
+      <section id="experience">
         <h1>Experience</h1>
-      </main>
-      <main id="contact">
+      </section>
+      <section id="contact">
         <h1>Contact</h1>
-      </main>
+      </section>
     </main>
   );
 }
